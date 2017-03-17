f38ee3870358d600e3c2f118e50b9648d6316290
Lucio Delelis <ldelelis@est.frba.utn.edu.ar>
2017-03-16 21:40:56 -0300

    Para alcanzar movimiento continuo, se reemplazó el switch por una cadena de if/elif.

--- main.cpp
+++ main.cpp
@@ -8,27 +8,30 @@ void inicializarNcurses();
 
 int main () {
   int xRows=0, yCols=0;
+  char curMovement;
   Init::inicializarNcurses(xRows, yCols);        // Arrancamos todas las funciones necesarias para iniciar el modo ncurses
   int keyPressed = 0;
   while (keyPressed != 27) {
     keyPressed = getch();
-    switch (keyPressed) {
-      case KEY_UP:
+    if (keyPressed == KEY_UP || curMovement == 'u') {
         --yCols;
+        curMovement = 'u';
         mvprintw(yCols, xRows, "flecha arriba\r");
-        break;
-      case KEY_DOWN:
+    }
+    else if (keyPressed == KEY_DOWN || curMovement == 'd') {
         ++yCols;
+        curMovement = 'd';
         mvprintw(yCols, xRows, "flecha abajo\r");
-        break;
-      case KEY_LEFT:
+    }
+    else if (keyPressed == KEY_LEFT || curMovement == 'l') {
         --xRows;
+        curMovement = 'l';
         mvprintw(yCols, xRows, "flecha izquierda\r");
-        break;
-      case KEY_RIGHT:
+    }
+    else if (keyPressed == KEY_RIGHT || curMovement == 'r') {
         ++xRows;
+        curMovement = 'r';
         mvprintw(yCols, xRows, "flecha derecha\r");
-        break;
     }
     refresh();
   }
