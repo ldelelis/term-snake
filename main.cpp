04c338402b47b2a9555ebd9e775b18a682835000
Lucio Delelis <ldelelis@est.frba.utn.edu.ar>
2017-03-17 17:59:41 -0300

    Se agregó control de cuadros (framerate).

--- main.cpp
+++ main.cpp
@@ -4,60 +4,66 @@
 #include "init.hpp"
 #include "snake.hpp"
 
+#define LIMIT_FRAMES 60000000
+
 void inicializarNcurses();
 
 int main () {
-  int xRows=0, yCols=0;
+  int xRows=0, yCols=0, keyPressed=0, CURRENT_FRAME=0;
   char curMovement;
   Init::inicializarNcurses(xRows, yCols);        // Arrancamos todas las funciones necesarias para iniciar el modo ncurses
-  int keyPressed = 0;
   while (keyPressed != 27) {
-    keyPressed = getch();
-    if (keyPressed == KEY_UP || curMovement == 'u') {
-        curMovement = 'u';
-        if (xRows != 0 && yCols != 0) {
-            --yCols;
-            mvprintw(yCols, xRows, "flecha arriba\r");
-        } else {
-          erase();
-          Init::inicializarAxis(xRows, yCols);
-          curMovement = 'a';
+    if (CURRENT_FRAME == LIMIT_FRAMES) {
+        CURRENT_FRAME = 0;
+        keyPressed = getch();
+        if (keyPressed == KEY_UP || curMovement == 'u') {
+            curMovement = 'u';
+            if (xRows != 0 && yCols != 0) {
+                --yCols;
+                mvprintw(yCols, xRows, "flecha arriba\r");
+            } else {
+              erase();
+              Init::inicializarAxis(xRows, yCols);
+              curMovement = 'a';
+            }
         }
-    }
-    else if (keyPressed == KEY_DOWN || curMovement == 'd') {
-        curMovement = 'd';
-        if (xRows != 0 && yCols != 0) {
-            ++yCols;
-            mvprintw(yCols, xRows, "flecha abajo\r");
-        } else {
-          erase();
-          Init::inicializarAxis(xRows, yCols);
-          curMovement = 'a';
+        else if (keyPressed == KEY_DOWN || curMovement == 'd') {
+            curMovement = 'd';
+            if (xRows != 0 && yCols != 0) {
+                ++yCols;
+                mvprintw(yCols, xRows, "flecha abajo\r");
+            } else {
+              erase();
+              Init::inicializarAxis(xRows, yCols);
+              curMovement = 'a';
+            }
         }
-    }
-    else if (keyPressed == KEY_LEFT || curMovement == 'l') {
-        curMovement = 'l';
-        if (xRows != 0 && yCols != 0) {
-            --xRows;
-            mvprintw(yCols, xRows, "flecha izquierda\r");
-        } else {
-          erase();
-          Init::inicializarAxis(xRows, yCols);
-          curMovement = 'a';
+        else if (keyPressed == KEY_LEFT || curMovement == 'l') {
+            curMovement = 'l';
+            if (xRows != 0 && yCols != 0) {
+                --xRows;
+                mvprintw(yCols, xRows, "flecha izquierda\r");
+            } else {
+              erase();
+              Init::inicializarAxis(xRows, yCols);
+              curMovement = 'a';
+            }
         }
-    }
-    else if (keyPressed == KEY_RIGHT || curMovement == 'r') {
-        curMovement = 'r';
-        if (xRows != 0 && yCols != 0) {
-            ++xRows;
-            mvprintw(yCols, xRows, "flecha derecha\r");
-        } else {
-          erase();
-          Init::inicializarAxis(xRows, yCols);
-          curMovement = 'a';
+        else if (keyPressed == KEY_RIGHT || curMovement == 'r') {
+            curMovement = 'r';
+            if (xRows != 0 && yCols != 0) {
+                ++xRows;
+                mvprintw(yCols, xRows, "flecha derecha\r");
+            } else {
+              erase();
+              Init::inicializarAxis(xRows, yCols);
+              curMovement = 'a';
+            }
         }
+        refresh();
+    } else {
+        CURRENT_FRAME += 1;
     }
-    refresh();
   }
   endwin();
   return 0;
