42f623b173530c10af5c700d942cba9312e4403d
Lucio Delelis <ldelelis@est.frba.utn.edu.ar>
2017-03-26 21:54:38 -0300

    Se agregaron múltiples comentarios al código y se borró un include innecesario (iostream)

--- src/main.cpp
+++ src/main.cpp
@@ -1,31 +1,29 @@
-#include <iostream>
 #include <ncurses.h>
 
 #include "init.hpp"
 #include "snake.hpp"
 
 int main () {
-  int xRows=0, yCols=0, lastX, lastY, keyPressed=0;
+  int xRows=0, yCols=0, lastX, lastY;
   char curMovement='a', lastMovement='a';
   Init::inicializarNcurses(xRows, yCols);        // Arrancamos todas las funciones necesarias para iniciar el modo ncurses
-  while (curMovement != 'e') {
+  while (curMovement != 'e') {                   // Mientras no hayamos apretado ESC
     lastMovement = curMovement;
-    curMovement = Init::inicializarTecla(curMovement, lastMovement);
-    keyPressed = getch();
-    if (curMovement == 'u') {
-      if (xRows != 0 && yCols != 0) {
-        lastX = xRows;
+    curMovement = Init::inicializarTecla(curMovement, lastMovement);     // Obtenemos el movimiento actual
+    if (curMovement == 'u') {                    // Arriba
+      if (xRows != 0 && yCols != 0) {            // Si no llegamos al borde...
+        lastX = xRows;                           // Obtenemos las posiciones anteriores...
         lastY = yCols;
-        --yCols;
-        mvprintw(yCols, xRows, "u\r");
-        mvprintw(lastY, lastX, " \r");
-      } else {
-        erase();
-        Init::inicializarAxis(xRows, yCols);
-        curMovement = 'a';
+        --yCols;                                 // ... realizamos el movimiento necesario...
+        mvprintw(yCols, xRows, "u\r");           // ... imprimimos el segmento de la serpiente...
+        mvprintw(lastY, lastX, " \r");           // ... y borramos el anterior
+      } else {                                   // Caso Contrario:
+        erase();                                 // Vaciamos completamente la pantalla...
+        Init::inicializarAxis(xRows, yCols);     // ... reinicializamos las posiciones en el centro...
+        curMovement = 'a';                       // Y dejamos el juego en hold hasta obtener un input de dirección.
       }
     }
-    else if (curMovement == 'd') {
+    else if (curMovement == 'd') {               // Abajo
       if (xRows != COLS && yCols != LINES) {
         lastX = xRows;
         lastY = yCols;
@@ -38,7 +36,7 @@ int main () {
         curMovement = 'a';
       }
     }
-    else if (curMovement == 'l') {
+    else if (curMovement == 'l') {               // Izquierda
       if (xRows != 0 && yCols != 0) {
         lastX = xRows;
         lastY = yCols;
@@ -51,7 +49,7 @@ int main () {
         curMovement = 'a';
       }
     }
-    else if (curMovement == 'r') {
+    else if (curMovement == 'r') {               // Derecha
       if (xRows != COLS  && yCols != LINES) {
         lastX = xRows;
         lastY = yCols;
@@ -64,10 +62,10 @@ int main () {
         curMovement = 'a';
       }
     }
-    refresh();
-    napms(300);
+    refresh();                                    // Flusheamos todo a la pantalla
+    napms(300);                                   // Pausa entre los ticks TODO: buscar una mejor alternativa si existe
   }
-  endwin();
+  endwin();                                       // Finalizamos el modo ncurses para evitar bugs con la terminal
   return 0;
 }
 
