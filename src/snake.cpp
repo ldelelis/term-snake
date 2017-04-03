6f64ae88b52b16cbf20d4b97e3a07a9c18366999
Lucio Delelis <ldelelis@est.frba.utn.edu.ar>
2017-04-03 20:40:46 -0300

    Se agregaron los métodos de incrementación y decrementación de posición.

--- src/snake.cpp
+++ src/snake.cpp
@@ -15,6 +15,22 @@ uint_t getY() {
   return this.yPos;
 }
 
+void incrementX() {
+  ++this.xPos;
+}
+
+void incrementY() {
+  ++this.yPos;
+}
+
+void decrementX() {
+  --this.xPos;
+}
+
+void decrementY() {
+  --this.yPos;
+}
+
 void moveSegmentsTick (uint_t tempLastX, uint_t tempLastY) {
   mvprintw(this.yPos, this.xPos, this.segment);
   mvprintw(this.lastXpos, this.lastYpos, " ");
