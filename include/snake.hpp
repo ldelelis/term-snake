bd9a01b8307bb2f69087dbeec52600cd01b37563
Lucio Delelis <ldelelis@est.frba.utn.edu.ar>
2017-04-04 19:36:54 -0300

    Redefinidos los métodos de 1 línea para modificar y retornar valores de posición.

--- include/snake.hpp
+++ include/snake.hpp
@@ -9,12 +9,12 @@ public:
   Snake (uint_t xPos, uint_t yPos) : xPos(600), yPos(600);
   void moveSegmentsTick();
   void moveLastSegment(uint_t lastX, uint_t lastY);
-  uint_t getX();
-  uint_t getY();
-  void incrementX();
-  void incrementY();
-  void decrementX();
-  void decrementY();
+  uint_t getX() { return this.xPos; }
+  uint_t getY() { return this.yPos; }
+  void incrementX() { ++this.xPos; }
+  void incrementY() { ++this.yPos; }
+  void decrementX() { --this.xPos; }
+  void decrementY() { --this.yPos; }
 };
 
 #endif
--- src/snake.cpp
+++ src/snake.cpp
@@ -8,30 +8,6 @@ Snake::Snake(uint_t lastX, uint_t lastY) {
   this.moveLastSegment(lastX, lastY);
 }
 
-uint_t getX() {
-  return this.xPos;
-}
-
-uint_t getY() {
-  return this.yPos;
-}
-
-void incrementX() {
-  ++this.xPos;
-}
-
-void incrementY() {
-  ++this.yPos;
-}
-
-void decrementX() {
-  --this.xPos;
-}
-
-void decrementY() {
-  --this.yPos;
-}
-
 void moveSegmentsTick (uint_t tempLastX, uint_t tempLastY) {
   mvprintw(this.yPos, this.xPos, this.segment);
   mvprintw(this.lastXpos, this.lastYpos, " ");
