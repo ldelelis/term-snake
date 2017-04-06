937c7e2e6f64b5308eb6ec1f40ab2904a09ffeb0
Lucio Delelis <ldelelis@est.frba.utn.edu.ar>
2017-04-05 21:12:52 -0300

    Expandido y cometado el metodo pseudocodigo para el movimiento e impresión de los segmentos

--- include/snake.hpp
+++ include/snake.hpp
@@ -19,7 +19,21 @@ public:
 
 #endif
 
+/*
 previo al metodo se mueve manualmente el primer segmento
 metodochotoparamoverse (vector serpiente, lastx, lasty) {
-  
+  int it=1;
+  for (it; it<=vivbora.size();it++) {
+    vibora.at(it).lastXPos = vibora.at(it).xPos;
+    vibora.at(it).lastYPos = vibora.at(it).YPos;
+    vibora.at(it).xPos = vibora.at(it-1).lastXPos;
+    vibora.at(it).yPos = vibora.at(it-1).lastYPos;
+    funcionchoteraparaimprimir(xPos, yPos, lastXPos, lastYPos);
+  }
 }
+
+funcionchoteraparaimprimir (xpos ypos lastxpos lastypos) {
+  mvprintw (this.segment+"\r", this.ypos, this.xpos);
+  mvprintw (" \r", this.lastypos, this.lastxpos);
+}
+*/
