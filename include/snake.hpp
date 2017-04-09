ef48176c9715ada95aac321a8980f1457580f52a
Lucio Delelis <ldelelis@est.frba.utn.edu.ar>
2017-04-09 15:52:14 -0300

    Se movieron los includes a la cabecera snake, se eliminaron métodos redundantes/inutiles, y se implementaron los métodos move y print necesarios.

--- include/snake.hpp
+++ include/snake.hpp
@@ -1,7 +1,9 @@
 #ifndef SNAKE_HPP
 #define SNAKE_HPP
 
+#include <ncurses.h>
 #include <string>
+#include <vector>
 
 class Snake {
 private:
@@ -9,33 +11,14 @@ private:
   unsigned int xPos, yPos, lastXPos, lastYPos;
 public:
   Snake (uint_t xPos, uint_t yPos) : xPos(600), yPos(600);
-  void moveSegmentsTick();
-  void moveLastSegment(uint_t lastX, uint_t lastY);
+  void printSegmentsTick();
   uint_t getX() { return this.xPos; }
   uint_t getY() { return this.yPos; }
   void incrementX() { ++this.xPos; }
   void incrementY() { ++this.yPos; }
   void decrementX() { --this.xPos; }
   void decrementY() { --this.yPos; }
+  void moveHead (char curMovement);
 };
 
 #endif
-
-/*
-previo al metodo se mueve manualmente el primer segmento
-metodochotoparamoverse (vector serpiente, lastx, lasty) {
-  int it=1;
-  for (it; it<=vivbora.size();it++) {
-    vibora.at(it).lastXPos = vibora.at(it).xPos;
-    vibora.at(it).lastYPos = vibora.at(it).YPos;
-    vibora.at(it).xPos = vibora.at(it-1).lastXPos;
-    vibora.at(it).yPos = vibora.at(it-1).lastYPos;
-    funcionchoteraparaimprimir(xPos, yPos, lastXPos, lastYPos);
-  }
-}
-
-funcionchoteraparaimprimir (xpos ypos lastxpos lastypos) {
-  mvprintw (this.segment+"\r", this.ypos, this.xpos);
-  mvprintw (" \r", this.lastypos, this.lastxpos);
-}
-*/
--- src/snake.cpp
+++ src/snake.cpp
@@ -1,5 +1,3 @@
-#include <ncurses.h>
-
 #include "snake.hpp"
 
 Snake::Snake(uint_t lastX, uint_t lastY) {
@@ -8,12 +6,39 @@ Snake::Snake(uint_t lastX, uint_t lastY) {
   this.moveLastSegment(lastX, lastY);
 }
 
-void moveSegmentsTick (uint_t tempLastX, uint_t tempLastY) {
-  mvprintw(this.yPos, this.xPos, this.segment);
-  mvprintw(this.lastXpos, this.lastYpos, " \r");
+void printSegmentsTick (std::string snakeVector, int it) {
+  mvprintw(snakeVector.at(it).yPos, snakeVector.at(it).xPos, snakeVector.segment);
+  mvprintw(snakeVector.at(it).lastXpos, snakeVector.at(it).lastYpos, " \r");
 }
 
-void moveLastSegment (uint_t lastX, uint_t lastY) {
-  this.x = lastX;
-  this.y = lastY;
+void moveSegmentsTick (std::vector snakeVector) {
+  int it=0;
+  for (it;it<=snakeVector.size(); it++) {
+    snakeVector.at(it).lastXPos = vibora.at(it).xPos;
+    snakeVector.at(it).lastYPos = vibora.at(it).yPos;
+    if (it > 0) {
+      snakeVector.at(it).xPos = snakeVector.at(it-1).lastXPos;
+      snakeVector.at(it).yPos = snakeVector.at(it-1).lastYPos;
+    } else {
+      this.moveHead(curMovement);
+    }
+    printSegmentsTick (snakeVector, it);
+  }
+}
+
+void moveHead (char curMovement) {
+  switch (curMovement) {
+    case 'u':
+      this.decrementY();
+      break;
+    case 'd':
+      this.incrementY();
+      break;
+    case 'l':
+      this.decrementX();
+      break;
+    case 'r':
+      this.incrementX();
+      break;
+  }
 }
