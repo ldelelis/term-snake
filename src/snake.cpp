ae401082ccba9e4fc84c6587368eaa4376bbd487
Lucio Delelis <ldelelis@est.frba.utn.edu.ar>
2017-04-15 13:46:19 -0300

    Corregido un conflicto con el metodo moveHead Shoutout a Captain-Redshirt en el discord r/argentina.

--- src/snake.cpp
+++ src/snake.cpp
@@ -4,16 +4,16 @@ void Snake::modifyHeadPos (char curMovement) {
   switch (curMovement) {
   case 'u':
     this->yPos-=1;
-    std::cout << this->yPos << std::endl;
+    break;
   case 'd':
     this->yPos+=1;
-    std::cout << this->yPos << std::endl;
+    break;
   case 'l':
     this->xPos-=1;
-    std::cout << this->xPos << std::endl;
+    break;
    case 'r':
     this->xPos+=1;
-    std::cout << this->xPos << std::endl;
+    break;
   default:
     return;
   }
