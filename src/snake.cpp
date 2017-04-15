567c49f715e4b91aae591abf20c3a1ed7c02b8d5
Lucio Delelis <ldelelis@est.frba.utn.edu.ar>
2017-04-15 13:28:44 -0300

    Me olvidé de agregar al repo los archivos de movement.

--- src/snake.cpp
+++ src/snake.cpp
@@ -4,12 +4,16 @@ void Snake::modifyHeadPos (char curMovement) {
   switch (curMovement) {
   case 'u':
     this->yPos-=1;
+    std::cout << this->yPos << std::endl;
   case 'd':
     this->yPos+=1;
+    std::cout << this->yPos << std::endl;
   case 'l':
     this->xPos-=1;
-  case 'r':
+    std::cout << this->xPos << std::endl;
+   case 'r':
     this->xPos+=1;
+    std::cout << this->xPos << std::endl;
   default:
     return;
   }
